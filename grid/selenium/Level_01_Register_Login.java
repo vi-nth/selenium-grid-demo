990be61c19293a3042434c82eba37ca98e441ab9
Nguyen Vi <hoangvi.due@gmail.com>
2022-09-05 20:52:49 +0700

    Update Level_01_Grid_Setting

--- grid/selenium/Level_01_Register_Login.java
+++ grid/selenium/Level_01_Register_Login.java
@@ -69,9 +69,8 @@ public class Level_01_Register_Login {
 	
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-
 		driver.get("https://demo.nopcommerce.com/");
-
+		
 		firstName = "Automation";
 		lastName = "FC";
 		email = "automation" + getRandomNumber() + "@us.com";
